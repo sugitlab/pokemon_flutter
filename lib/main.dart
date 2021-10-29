c75d407f4e50122ee9773943274bd6f219748243
Sugit <sgmt.snj@gmail.com>
2021-10-29 23:45:47 +0900

    pokemon view

--- lib/main.dart
+++ lib/main.dart
@@ -22,13 +22,42 @@ class MyHomePage extends StatelessWidget {
   const MyHomePage({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: Text(
-          'Hello World',
-          style: TextStyle(
-            fontSize: 36,
-          ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Stack(
+              children: [
+                Container(
+                  padding: const EdgeInsets.all(32),
+                  child: Image.network(
+                    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+                    height: 100,
+                    width: 100,
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.all(8),
+                  child: const Text(
+                    'No.25',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const Text(
+              'pikachu',
+              style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+            ),
+            const Chip(
+              backgroundColor: Colors.yellow,
+              label: Text('electric'),
+            ),
+          ],
         ),
       ),
     );
